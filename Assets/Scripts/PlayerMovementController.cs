b735ebde66feeee5bb822f1f003d0724b34d6518
nouaxs <74322389+nouaxs@users.noreply.github.com>
2023-03-09 01:24:06 +0000

    adjusted code

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -7,8 +7,7 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour
 {
 
-    [SerializeField] private float stepsInterval = 0.2f;
-    //private const float STEP_SIZE = 2f;
+    [SerializeField] private float stepsInterval = 0.2f;  // time interval in seconds between each step when holding a key down
     private Rigidbody2D rb;
     private bool isMoving;
     private Vector3 curPosi, nextPosi;
@@ -33,30 +32,27 @@ public class PlayerMovementController : MonoBehaviour
         Vector3 moveDirection = Vector3.zero;
         Quaternion charRotation = transform.rotation;
         if((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && !isMoving) {   
-            //StartCoroutine(UpdatePosition(Vector3.up));
             moveDirection += Vector3.up;
             charRotation = Quaternion.Euler(0f,0f,0f);
 
         }
         
         if((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && !isMoving) {  
-            //StartCoroutine(UpdatePosition(Vector3.left));
             moveDirection += Vector3.left;
-            charRotation = Quaternion.Euler(0f,0f,90f);
+            //charRotation = Quaternion.Euler(0f,0f,90f);
+            rb.SetRotation(90);
             
 
         } 
         
         if((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && !isMoving) {  
-            //StartCoroutine(UpdatePosition(Vector3.right));
             moveDirection += Vector3.right;
             charRotation = Quaternion.Euler(0f,0f,-90f);
             
 
         } 
         
-        if((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) && !isMoving) {  
-            //StartCoroutine(UpdatePosition(Vector3.down));
+        if((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) && !isMoving) {
             moveDirection += Vector3.down;
             charRotation = Quaternion.Euler(0f,0f,180f);
         }
@@ -73,7 +69,7 @@ public class PlayerMovementController : MonoBehaviour
         isMoving = true;
         float elapsedTime = 0f;
         curPosi = transform.position;
-        nextPosi = curPosi + target;
+        nextPosi = curPosi + target; //the final location we want to reach
         float journeyLength = Vector3.Distance(curPosi, nextPosi);
 
         while(elapsedTime < stepsInterval) {
@@ -87,7 +83,6 @@ public class PlayerMovementController : MonoBehaviour
         }
 
         transform.position = nextPosi;
-
         isMoving = false;
 
     }
