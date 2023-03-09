38fc8dfb444630edae8fa77f3ca7158659a83df7
nouaxs <74322389+nouaxs@users.noreply.github.com>
2023-03-09 00:07:14 +0000

    Final movement implemented (before collision checks)

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -31,17 +31,18 @@ public class PlayerMovementController : MonoBehaviour
 
     private void MovePlayer() {
         Vector3 moveDirection = Vector3.zero;
+        Quaternion charRotation = transform.rotation;
         if((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && !isMoving) {   
             //StartCoroutine(UpdatePosition(Vector3.up));
             moveDirection += Vector3.up;
-            transform.rotation = Quaternion.Euler(0f,0f,0f);
+            charRotation = Quaternion.Euler(0f,0f,0f);
 
         }
         
         if((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && !isMoving) {  
             //StartCoroutine(UpdatePosition(Vector3.left));
             moveDirection += Vector3.left;
-            transform.rotation = Quaternion.Euler(0f,0f,90f);
+            charRotation = Quaternion.Euler(0f,0f,90f);
             
 
         } 
@@ -49,7 +50,7 @@ public class PlayerMovementController : MonoBehaviour
         if((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && !isMoving) {  
             //StartCoroutine(UpdatePosition(Vector3.right));
             moveDirection += Vector3.right;
-            transform.rotation = Quaternion.Euler(0f,0f,-90f);
+            charRotation = Quaternion.Euler(0f,0f,-90f);
             
 
         } 
@@ -57,12 +58,12 @@ public class PlayerMovementController : MonoBehaviour
         if((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) && !isMoving) {  
             //StartCoroutine(UpdatePosition(Vector3.down));
             moveDirection += Vector3.down;
-            transform.rotation = Quaternion.Euler(0f,0f,180f);
+            charRotation = Quaternion.Euler(0f,0f,180f);
         }
 
         if (moveDirection != Vector3.zero)
         {
-            
+            transform.rotation = charRotation;
             StartCoroutine(UpdatePosition(moveDirection));
         }
     }
@@ -90,74 +91,4 @@ public class PlayerMovementController : MonoBehaviour
         isMoving = false;
 
     }
-
-    // private void MovePlayer()
-    // {
-    //     float horizontalInput = Input.GetAxisRaw("Horizontal");
-    //     float verticalInput = Input.GetAxisRaw("Vertical");
-
-        
-    //     // Calculate the direction vector based on the input axes
-    //     //Vector3 direction = new Vector3(horizontalInput, verticalInput, 0f );
-    //    // direction = direction.normalized * stepsInterval * Time.deltaTime;
-    //     //rb.MovePosition(rb.transform.position + direction);
-
-    //     // // Normalize the direction vector to ensure consistent movement speed
-    //     // if (direction.magnitude > 1f)
-    //     // {
-    //     //     direction.Normalize();
-    //     // }
-
-    //     // // Move the player based on the direction vector and speed
-    //     // transform.Translate(direction * stepsInterval * Time.deltaTime);
-
-    //     // if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
-    //     //     UpdatePosition(Vector3.up);
-    //     // }
-
-    //     // transform.position = Vector3.MoveTowards(transform.position, middlePoint.position, stepsInterval * Time.deltaTime);
-
-    //     // if (Mathf.Abs(horizontalInput) == 1f) 
-    //     // {
-    //     //     middlePoint.position += new Vector3(horizontalInput, 0f, 0f );
-    //     // }
-
-    //     // if (Mathf.Abs(verticalInput) == 1f) 
-    //     // {
-    //     //     middlePoint.position += new Vector3(0f, verticalInput, 0f );
-    //     // }
-
-    //     // Set movement direction
-    //     posi.x = Mathf.Round(horizontalInput);
-    //     posi.z = Mathf.Round(verticalInput);
-
-    //     // Normalize movement direction
-    //     posi = posi.normalized;
-
-    //     // Move player by stepSize in movement direction
-    //     transform.position += posi * STEP_SIZE * stepsInterval * Time.deltaTime;
-
-    // }
-
-    // private void UpdatePosition(Vector3 direction) 
-    // {
-    //     Vector3 position = rb.position;
-    // }
-
-    //  private void Move(Vector3 direction) {
-    //     rb = GetComponent<Rigidbody2D>();
-    //     Vector3 position = rb.position;
-    //     Vector3 nextStep =  transform.position + direction;
-
-    //     Collider2D barrier = Physics2D.OverlapBox(nextStep, Vector2.zero, 0f, LayerMask.GetMask("Barrier"));
-
-
-    //     if (barrier != null) {
-    //          transform.position = transform.position;
-    //     } else {
-    //         transform.position += direction;
-    //     }
-    // }
-
-
 }
