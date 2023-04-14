aa5ecbcbc543b6bc32752e65ea50ce7bed73516b
nouaxs <74322389+nouaxs@users.noreply.github.com>
2023-04-14 15:58:54 +0100

    added car destroying mechanics

--- Assets/Scripts/Cars/CarSpawner.cs
+++ Assets/Scripts/Cars/CarSpawner.cs
@@ -4,24 +4,32 @@ using UnityEngine;
 
 public class CarSpawner : MonoBehaviour
 {
-    [SerializeField] float spawnInterval;
+    [SerializeField] float spawnInterval; // Duration between each car spawn action
     [SerializeField] GameObject carObject;
     [SerializeField] bool isGameActive = true;
     [SerializeField] Transform[] spawnPoints;
+    private const float timeToDestroy = 10f;
 
     private void Start()
     {
         StartCoroutine(SpawnCars());
     }
 
+    /**
+     * This method spawns cars at random spawn points and then destroys then after a certain amount of time
+     */
     IEnumerator SpawnCars()
     {
-        while(isGameActive == true)
+        while (isGameActive == true)
         {
             int randomIndx = Random.Range(0, spawnPoints.Length);
             Transform spawnPoint = spawnPoints[randomIndx];
             GameObject spawnedCar = Instantiate(carObject, spawnPoint.position, spawnPoint.rotation);
             yield return new WaitForSeconds(spawnInterval);
+
+            Destroy(spawnedCar, timeToDestroy);
         }
     }
+
+
 }
